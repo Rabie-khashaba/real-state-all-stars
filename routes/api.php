8c68edda2018ea2e93476722ccd80faa73f3a875
Rabie-khashaba <khashabarabie@gmail.com>
2026-03-26 21:46:23 +0200

    add update

--- routes/api.php
+++ routes/api.php
@@ -34,6 +34,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('/login', [AuthController::class, 'login']);
+
+
 Route::get('/about', [AboutController::class, 'index']);
 
 Route::prefix('judges')->group(function () {
